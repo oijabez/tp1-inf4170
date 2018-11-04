6852848ec3fadcb294b89ec399b37155e1fbd119
Jabez Adasz <oijabez@gmail.com>
2018-11-04 17:12:10 -0500

    refactoring tp1.java

--- tp1.java
+++ tp1.java
@@ -7,11 +7,12 @@
  */
 
 import java.util.Scanner;
+
 public class tp1 {
 
     static int[] a;
     static int[] b;
-    static Scanner sc = new Scanner (System.in);
+    static Scanner sc = new Scanner(System.in);
 
     /**
      *
@@ -30,9 +31,8 @@ public class tp1 {
      * @return
      */
     static int getLeftChildIndex(int index) {
-        return Math.max(2 * index + 1, a.length-1);
+        return Math.max(2 * index + 1, a.length - 1);
     }
-
 
     /**
      *
@@ -40,21 +40,19 @@ public class tp1 {
      * @return
      */
     static int getRightChildIndex(int index) {
-        return Math.max(2 * index + 2, a.length-1);
+        return Math.max(2 * index + 2, a.length - 1);
     }
-
 
     /**
      *
      * @param a
      */
-    static void afficherTableau(int[] a, int n){
+    static void afficherTableau(int[] a, int n) {
         System.out.println("Voici le contenu du tableau: ");
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.print(a[i] + "  ");
         }
     }
-
 
     /**
      *
@@ -64,48 +62,40 @@ public class tp1 {
     public static void sort(int[] a, int par_taille) {
 
         heapify(a);
-        for (int i = par_taille-1; i > 0; i--)
-        {
+        for (int i = par_taille - 1; i > 0; i--) {
             swap(0, i);
-            par_taille = par_taille-1;
+            par_taille = par_taille - 1;
             maxheap(a, 0);
         }
     }
 
-
     /* Function to build a heap */
-    public static void heapify(int arr[])
-    {
-        int N = arr.length-1;
-        for (int i = N/2; i >= 0; i--)
+    public static void heapify(int arr[]) {
+        int N = arr.length - 1;
+        for (int i = N / 2; i >= 0; i--)
             maxheap(arr, i);
     }
-
-
 
     /**
      *
      * @param rootIndex
      * @param lastIndex
      */
-    static void fixHeap(int rootIndex, int lastIndex){
+    static void fixHeap(int rootIndex, int lastIndex) {
         b = new int[lastIndex - rootIndex + 1];
         int j = 0;
 
-        for(int i = rootIndex ; i < lastIndex; i++){
+        for (int i = rootIndex; i < lastIndex; i++) {
             b[j] = a[i];
-            j ++;
+            j++;
         }
 
     }
-    
-
 
     /* Function to swap largest element in heap */
-    public static void maxheap(int arr[], int i)
-    {
-        int N = arr.length-1;
-        int left = getLeftChildIndex(i) ;
+    public static void maxheap(int arr[], int i) {
+        int N = arr.length - 1;
+        int left = getLeftChildIndex(i);
         int right = getRightChildIndex(i);
         int max = i;
         if ((left <= N) && (arr[left] > arr[i]))
@@ -113,47 +103,43 @@ public class tp1 {
         if ((right <= N) && (arr[right] > arr[max]))
             max = right;
 
-        if (max != i)
-        {
+        if (max != i) {
             swap(i, max);
             maxheap(arr, max);
         }
     }
 
-
-
-
     /**
      *
      * @param args
      */
-    public static void main (String args []) {
-
+    public static void main(String args[]) {
 
         System.out.println("Veuillez saisir la taille du tableau: ");
         int n = sc.nextInt();
 
-        while(n<=0){
+        while (n <= 0) {
             System.out.println("Vous devez saisir un entier positif! Recommencez ... ");
             n = sc.nextInt();
         }
 
         a = new int[n];
-        for(int i=0;i<n;i++){
-            a[i] = (int)(Math.random()*1000);
+        for (int i = 0; i < n; i++) {
+            a[i] = (int) (Math.random() * 1000);
         }
 
         afficherTableau(a, n);
 
-        //Appels des méthodes pour le numéro 1
+        // Appels des méthodes pour le numéro 1
         System.out.println("\n\n*** Échange du contenu des éléments du tableau ***");
-        System.out.println("Veuillez saisir deux entiers i et j qui représentent les positions des deux éléments a echanger dans le tableau:");
-        System.out.println("i et j doivent être compris entre 0 et " + (n-1));
+        System.out.println(
+                "Veuillez saisir deux entiers i et j qui représentent les positions des deux éléments a echanger dans le tableau:");
+        System.out.println("i et j doivent être compris entre 0 et " + (n - 1));
         int i = sc.nextInt();
         int j = sc.nextInt();
 
-        while((i<0) || (j<0) || (i>=n) || (j>=n)) {
-            System.out.println("Vous devez saisir deux entiers entre 0 et " + (n-1) + " ! Recommencez ... ");
+        while ((i < 0) || (j < 0) || (i >= n) || (j >= n)) {
+            System.out.println("Vous devez saisir deux entiers entre 0 et " + (n - 1) + " ! Recommencez ... ");
             i = sc.nextInt();
             j = sc.nextInt();
         }
@@ -161,37 +147,33 @@ public class tp1 {
         swap(i, j);
         afficherTableau(a, n);
 
-
         System.out.println("\n\n*** Retourner l'indice de l'enfant a gauche ***");
         System.out.println("Veuillez saisir un entier qui représente un index d'un élément dans le tableau:");
-        System.out.println("index doit être compris entre 0 et " + (n-1));
+        System.out.println("index doit être compris entre 0 et " + (n - 1));
         int index = sc.nextInt();
 
-        while((index<0) || (index>=n)) {
-            System.out.println("Vous devez saisir un entier entre 0 et " + (n-1) + " ! Recommencez ... ");
+        while ((index < 0) || (index >= n)) {
+            System.out.println("Vous devez saisir un entier entre 0 et " + (n - 1) + " ! Recommencez ... ");
             index = sc.nextInt();
         }
 
         int gauche = getLeftChildIndex(index);
         System.out.println("l'indice de l'enfant gauche est = " + gauche);
 
-
-
         System.out.println("\n\n*** Retourner l'indice de l'enfant a droite ***");
         System.out.println("Veuillez saisir un entier qui représente un index d'un élément dans le tableau:");
-        System.out.println("index doit être compris entre 0 et " + (n-1));
+        System.out.println("index doit être compris entre 0 et " + (n - 1));
         index = sc.nextInt();
 
-        while((index<0) || (index>=n)) {
-            System.out.println("Vous devez saisir un entier entre 0 et " + (n-1) + " ! Recommencez ... ");
+        while ((index < 0) || (index >= n)) {
+            System.out.println("Vous devez saisir un entier entre 0 et " + (n - 1) + " ! Recommencez ... ");
             index = sc.nextInt();
         }
 
         int droite = getRightChildIndex(index);
         System.out.println("l'indice de l'enfant droite est = " + droite);
 
-
-        //Tri du tableau
+        // Tri du tableau
         sort(a, n);
         afficherTableau(a, n);
 
